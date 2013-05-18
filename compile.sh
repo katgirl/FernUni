500f731f0bfc6b698a25439379cb98a4e4dac642
Kirsten Roschanski <kirsten@kat-webdesign.de>
2013-05-18 04:13:15 +0200

    Template
    Signed-off-by: Kirsten Roschanski <kirsten@kat-webdesign.de>

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# runme if you want to update all your abbreviations
+
+pdflatex Hauptdatei.tex
+
+mv Hauptdatei.pdf 21817-ausarbeitung01-9000000,9053522-mahlecke,roschanski-Aspekte_der_Sicherheit_in_der_Programmierung.pdf
