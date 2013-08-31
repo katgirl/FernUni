56793cba4cb0d97247f25f9fbabd9e4b3ac1055f
Kirsten Roschanski <kirsten@kat-webdesign.de>
2013-08-31 23:01:30 +0200

    Literaturverzeichnis nit aufgenommen

--- compile.sh
+++ compile.sh
@@ -2,5 +2,9 @@
 # runme if you want to update all your abbreviations
 
 pdflatex Hauptdatei.tex
+bibtex Hauptdatei.aux
+pdflatex Hauptdatei.tex
+pdflatex Hauptdatei.tex
+
 
 mv Hauptdatei.pdf 21817-ausarbeitung01-8632014,9053522-mahlecke,roschanski-Aspekte_der_Sicherheit_in_der_Programmierung.pdf
